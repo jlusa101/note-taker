76fa2097f6d799ce81cac97ec2373858021dc409
Joona Lusa <joona.lusa@hotmail.com>
2021-12-30 19:48:19 -0500

    server is up and running, brings up index.html

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 3504;
+
+const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+app.use(express.static('public'));
+
+// Routing the server to send index.html to the user's screen
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+// Routing user to the notes.html to view their notes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+// Listening on the specified port and informing user
+app.listen(PORT, () => {
+    console.log(`API server listening on port ${PORT}!`);
+});
