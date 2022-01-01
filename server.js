614baf2dbf4d6ad920c6a74b71549e6bb0d1c956
Joona Lusa <joona.lusa@hotmail.com>
2022-01-01 16:36:27 -0500

    saved notes in the database can be viewed in browser

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const { notes } = require("./db/notes.json");
 
-const PORT = process.env.PORT || 3504;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
